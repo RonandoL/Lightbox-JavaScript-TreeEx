f44f09d60ea1237c19087a5aa7867c0ce08a7b9f
Ronando Long <ronando.long@gmail.com>
2016-03-04 10:03:11 -0800

    Tweaked.

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,10 @@
 
 var $overlay = $('<div id="overlay"></div');
 var $image = $('<img>');
-var $caption = $('<p></p>');
+var $caption = $('<h3></h3>');
 
 $overlay.append($image);
 $('body').append($overlay);
-  // add caption
 $overlay.append($caption);
 
 // capture click event on link to image
@@ -15,17 +14,26 @@ $('#imageGallery a').click(function(event) {
   event.preventDefault();
 
   var imageLocation = $(this).attr('href');
-  $image.attr("src", imageLocation);
+  $image.attr('src', imageLocation);
 
-  // show overlay
   $overlay.show();
 
-  // add alt tag (image is child of the link)
   var captionText = $(this).children('img').attr('alt');
   $caption.text(captionText);
 });
 
-// when overlay is clicked
 $overlay.click(function() {
   $overlay.hide();
 });
+
+
+
+
+
+
+
+
+
+
+
+//
