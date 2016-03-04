d6bc9ad104b3a800ebcb87de2bdf6a83231a03ae
Ronando Long <ronando.long@gmail.com>
2016-03-03 18:12:26 -0800

    Added images and files

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,31 @@
-// Business Logic
-// var helloWorld = function(){
-//   return false;
-//
-//   For loops and shit go here
-//   If/else like a madman here
-//
-//  return shit like a mo-fo here
-//
-//   ...whatchyu lookin at bitch? Green those specs and git back to work!
-//  Time fo yo Interface Logic, then suckface when it's all green.
-// };
+//Problem: User when clicking on image goes to a dead end
+//Solution: Create an overlay with the large image - Lightbox
 
+var $overlay = $('<div id="overlay"></div');
+var $image = $('<img>');
+var $caption = $('<p></p>');
 
+$overlay.append($image);
+$('body').append($overlay);
+  // add caption
+$overlay.append($caption);
 
+// capture click event on link to image
+$('#imageGallery a').click(function(event) {
+  event.preventDefault();
 
+  var imageLocation = $(this).attr('href');
+  $image.attr("src", imageLocation);
 
+  // show overlay
+  $overlay.show();
 
+  // add alt tag (image is child of the link)
+  var captionText = $(this).children('img').attr('alt');
+  $caption.text(captionText);
+});
 
-
-// User Interface Logic
-// $(document).ready(function() {
-//   $("form#IDselector").submit(function(event) {
-//     // variables and shit go here, bitch!
-//
-//     // link your muthafuckin' variables to yo bad-ass bidness logic function, Yo!
-//
-//     // Make shit happen here...BITCH!
-//
-//     // Muthafuckin' variables connect to your output down here hommie.
-//
-//     // Show me some money, hunny, and reveal that hiddin fine shit you got stashed.
-//
-//     // It's all good baby.'
-//     event.preventDefault();
-//   });
-// });
+// when overlay is clicked
+$overlay.click(function() {
+  $overlay.hide();
+});
